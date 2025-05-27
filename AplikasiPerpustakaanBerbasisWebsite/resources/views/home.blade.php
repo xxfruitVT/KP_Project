3a947f6e96b84b3f829ff833c3486cf0ceb8b9a8
Angga Afriliansyah <145992666+xxfruitVT@users.noreply.github.com>
2025-05-27 07:19:44 +0700

    up

---
+++ AplikasiPerpustakaanBerbasisWebsite/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h2>Selamat Datang di Website Resmi SMAN 78 Jakarta</h2>
+        <iframe width="100%" height="480" src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allowfullscreen></iframe>
+    </div>
+@endsection
---
+++ AplikasiPerpustakaanBerbasisWebsite/resources/views/tabs/eperpus.blade.php
@@ -0,0 +1,2 @@
+<h3>E-Perpus</h3>
+<p>Akses ke perpustakaan digital SMAN 78 Jakarta.</p>

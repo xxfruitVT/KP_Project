470721abad1456c1b20d8bb50deed78e28624b0e
Angga Afriliansyah <145992666+xxfruitVT@users.noreply.github.com>
2025-05-28 07:20:03 +0700

    up

--- AplikasiPerpustakaanBerbasisWebsite/routes/web.php
+++ AplikasiPerpustakaanBerbasisWebsite/routes/web.php
@@ -6,8 +6,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Login routes
+Route::get('/login/admin', function () {
+    return 'Halaman Login Admin';
+})->name('login.admin');
+Route::get('/login/student', function () {
+    return 'Halaman Login Siswa';
+})->name('login.student');
+Route::get('/register', function () {
+    return 'Halaman Registrasi';
+})->name('register');
+// Route login default (optional)
+Route::get('/login', function () {
+    return redirect()->route('login.student');
+})->name('login');
+
 Route::get('/', function () {
     return view('layouts.app');
 });
-
-
